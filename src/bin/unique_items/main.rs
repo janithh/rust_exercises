8ef8eb895c930fba0504ea28dfb4b54cc99501a4
janithh <janithharendra@gmail.com>
2022-10-19 21:16:07 +0530

    add exercise 2

--- src/bin/unique_items/main.rs
+++ src/bin/unique_items/main.rs
@@ -1,8 +1,9 @@
 mod unique_items;
 
 fn main() {
-
-    unique_items::exercise::find_unique();
+    let input_list : Vec<i32> = vec![1, 4, 1, 5, 5];
+    let unique_list = unique_items::exercise::get_unique(&input_list);
+    println!("[unique_items::main] Status - {:?}", unique_list);
 }
 
 
--- src/bin/unique_items/unique_items.rs
+++ src/bin/unique_items/unique_items.rs
@@ -3,8 +3,20 @@
  *  Exercise
  *******************/
 pub mod exercise {
-    pub fn find_unique() {
+    use std::cmp;
 
+    pub fn get_unique<T>(input_list: &Vec<T>) -> Vec<T> 
+        where T: cmp::PartialOrd + Copy {
+        let mut unique_list: Vec<T> = Vec::new();
+
+        for element in input_list {
+            match unique_list.iter().find(|&&x| x == *element) {
+                None => unique_list.push(element.clone()),
+                Some(_) => ()
+            };
+        }
+
+        unique_list
     }
 }
 
@@ -13,5 +25,45 @@ pub mod exercise {
  *******************/
 #[cfg(test)]
 mod test {
+    use super::exercise;
+
+    #[test]
+    fn ut_get_unique_sorted_unique_input() {
+        let input_list : Vec<i32> = vec![1, 4, 5];
+        let unique_list = exercise::get_unique(&input_list);
+
+        assert_eq!(unique_list, input_list);
+    }
+
+    #[test]
+    fn ut_get_unique_sorted_dup_input() {
+        let input_list : Vec<i32> = vec![1, 1, 3];
+        let unique_list = exercise::get_unique(&input_list);
+
+        assert_eq!(unique_list, vec![1, 3]);
+    }
     
+    #[test]
+    fn ut_get_unique_unsorted_unique_input() {
+        let input_list : Vec<i32> = vec![5, 1, 4];
+        let unique_list = exercise::get_unique(&input_list);
+
+        assert_eq!(unique_list, input_list);
+    }
+
+    #[test]
+    fn ut_get_unique_unsorted_dup_input() {
+        let input_list : Vec<i32> = vec![1, 3, 1];
+        let unique_list = exercise::get_unique(&input_list);
+
+        assert_eq!(unique_list, vec![1, 3]);
+    }
+
+    #[test]
+    fn ut_get_unique_empty_input() {
+        let input_list : Vec<i32> = vec![];
+        let unique_list = exercise::get_unique(&input_list);
+
+        assert_eq!(unique_list, vec![]);
+    }   
 }
